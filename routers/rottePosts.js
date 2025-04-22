4c702698a3253626f086647997d512568d5f9657
mauroanastasi <mauroanastasi1@gmail.com>
2025-04-22 16:00:51 +0200

    [UPD] rotte e esporto il router

---
+++ routers/rottePosts.js
@@ -0,0 +1,38 @@
+// importo express
+const express = require(`express`);
+
+// importo la classe router
+const router = express.Router();
+
+// index    
+router.get('/', function (req, res) {
+    res.send(`Elenco dei post`);
+});
+    
+// show
+router.get('/:id', function (req, res) {
+    res.send('Dettagli dei posts ' + req.params.id);
+});
+    
+// store
+router.post('/', function (req, res) {
+    res.send('Creazione nuovi posts');
+});
+    
+// update
+router.put('/:id', function (req, res) {
+    res.send('Modifica integrale dei posts ' + req.params.id);
+});
+    
+// modify
+router.patch('/:id', function (req, res) {
+    res.send('Modifica parziale dei posts ' + req.params.id);
+});
+    
+// destroy
+router.delete('/:id', function (req, res) {
+    res.send('Eliminazione dei posts ' + req.params.id);
+});
+
+// esporto il router
+module.exports = router;
